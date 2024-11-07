deabc6eadab472b78aa190ad952587eff47b5177
Panna Liptak <liptakpanna@gmail.com>
2024-11-07 17:19:12 +0100

    rename create branch

--- invenio_preservation_sync/alembic/7983d81b23cb_create_preservation_info_branch.py
+++ invenio_preservation_sync/alembic/7983d81b23cb_create_preservation_info_branch.py
@@ -5,7 +5,7 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Create branch."""
+"""Create Preservation Info branch."""
 
 import sqlalchemy as sa
 from alembic import op
